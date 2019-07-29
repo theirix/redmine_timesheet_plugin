bcd7be06dcb92bb100f681538b92b0ffd64d28a3
theirix <theirix@gmail.com>
2019-07-29 23:17:32 +0300

    Specify exact Rails version in migration

--- db/migrate/002_create_enum_prices.rb
+++ db/migrate/002_create_enum_prices.rb
@@ -1,4 +1,4 @@
-class CreateEnumPrices < ActiveRecord::Migration
+class CreateEnumPrices < (Rails.version < '5.1') ? ActiveRecord::Migration : ActiveRecord::Migration[4.2]
   def change
     create_table :prices do |t|
       t.references :enumerations
